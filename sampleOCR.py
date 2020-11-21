656fba8621214bc869d223dc27b4035d97ab74fd
abisola akinrinade <bisola_akin@yahoo.com>
2020-11-21 05:51:57 -0700

    improved processing

--- sampleOCR.py
+++ sampleOCR.py
@@ -4,14 +4,17 @@ import PyPDF2
 import numpy as np
 import pdf2image
 import cv2
+from imutils.object_detection import non_max_suppression
 import tempfile
+
+import sklearn as sklearn
 from matplotlib import pyplot as plt
 import os
-from skimage import io, color
-from scipy.fftpack import dct, idct
-from skimage import filters, morphology, measure
+
+from skimage import filters, morphology, measure, transform
 from scipy.ndimage.morphology import binary_fill_holes
-from skimage import transform
+from sklearn import cluster
+from scipy.fftpack import dct, idct
 
 """"
 sample = pdf2image.convert_from_path('C:\\Users\\bisib\\PycharmProjects\\SampleOCR\\sample_data\\Scanned_20200928-1633.pdf')
@@ -46,12 +49,12 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 """
 #cv2.imwrite('C:/Temp/person-masked.jpg', masked)           # Save
 
-from PIL import ImageFilter, Image
+from PIL import ImageFilter, Image, ImageEnhance, ImageOps
+
 pt.pytesseract.tesseract_cmd ="C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 files = os.listdir('testingdata')
 path = os.getcwd()
 for file in files:
-
 
     filepath = os.path.join(path, 'testingdata', file)
     if str(file).endswith('.pdf'):
@@ -62,17 +65,66 @@ for file in files:
 
     # first of many steps of image preprocessing
     im = Image.open(filepath)
+    orig = im.copy()
+    orig = np.array(orig)
+    im = ImageEnhance.Contrast(im).enhance(1.2)
+    imm = ImageEnhance.Contrast(im).enhance(1.2)
+    imm = ImageOps.grayscale(imm)
+
+    frequencies = dct(dct(imm, axis=0), axis=1)
+    frequencies[:2, :2] = 0
+    gray = idct(idct(frequencies, axis=1), axis=0)
+
+    gray = (gray - gray.min()) / (gray.max() - gray.min())  # renormalize to range [0:1]
+    plt.subplot(121), plt.imshow(im)
+    plt.subplot(122), plt.imshow(gray)
+    plt.show()
     # blur
-    im1 = im.filter(ImageFilter.BLUR)
+    im1 = imm.filter(ImageFilter.BLUR)
+
     # filter
-    im2 = im.filter(ImageFilter.MinFilter(3))
-    im3 = im.filter(ImageFilter.MinFilter)
+    im2 = imm.filter(ImageFilter.MinFilter(3))
+    im3 = imm.filter(ImageFilter.MinFilter)
     plt.subplot(121),plt.imshow(im)
     plt.subplot(122),plt.imshow(im3)
     plt.show()
+    #open cv
+    openCVim = np.array(im3)
+    treated = cv2.fastNlMeansDenoising(openCVim, None, 10, 10, 7)
+
+    thresh = cv2.threshold(treated, 150, 255, cv2.THRESH_BINARY)[1]
+    plt.subplot(121), plt.imshow(treated)
+    plt.subplot(122), plt.imshow(thresh)
+    plt.show()
+    #treated = cv2.fastNlMeansDenoising(treated, None, 10, 10, 7)
+    #treated = cv2.cvtColor(treated, cv2.COLOR_RGB2GRAY)
+    kernel = np.ones((5, 5), np.uint8)
+    treated = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+
+
+
+    treated = cv2.bitwise_not(treated)
+    plt.subplot(121), plt.imshow(openCVim)
+    plt.subplot(122), plt.imshow(treated)
+    plt.show()
+    treated2 = cv2.morphologyEx(treated, cv2.MORPH_CLOSE, kernel)
+    plt.subplot(121), plt.imshow(treated)
+    plt.subplot(122), plt.imshow(treated2)
+    plt.show()
+
+    # find contours
+
+
+    mask = np.ones((treated2.shape[0],treated2.shape[1]), np.uint8)
+    masked = cv2.bitwise_or(treated2, treated2, mask=mask)
+    transposed = cv2.cvtColor(masked,cv2.COLOR_GRAY2RGB)
+    transposed = cv2.bitwise_not(transposed)
+    plt.subplot(121), plt.imshow(masked)
+    plt.subplot(122), plt.imshow(transposed)
+    plt.show()
     # read text
-    image_data = pt.image_to_data(im3)
-    txt1 = pt.image_to_string(im3)
+    image_data = pt.image_to_data(treated)
+    txt1 = pt.image_to_string(treated)
     #txt_original = pt.image_to_string(im)
     filename = str(file)+'.txt'
     textpath = os.path.join(path, 'testingoutput', filename)
